d4e4735fe79a90310ae43142010ac71ed29740ff
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-03-20 16:28:28 +0100

    init

---
+++ app/src/main/java/com/projekt_zespolowy/microclimateanalysisclient/MainActivity.java
@@ -0,0 +1,14 @@
+package com.projekt_zespolowy.microclimateanalysisclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
